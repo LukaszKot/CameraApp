1deeb27e35ded19b25f49fbab800572fca57e0d7
lukaszkot03@gmail.com <lukaszkot03@gmail.com>
2019-11-28 00:30:10 +0100

    Added marking photos to delete.

--- testapp004/components/Gallery.js
+++ testapp004/components/Gallery.js
@@ -51,7 +51,7 @@ class Gallery extends Component {
                 <View style={styles.galleryView}>
                     <FlatList style={{ flexDirection: 'column' }}
                         data={this.state.photos}
-                        renderItem={({ item }) => <FotoItem isGrid={this.state.isGrid} photo={item} />}
+                        renderItem={({ item, index }) => <FotoItem isGrid={this.state.isGrid} photo={item} selectCallback={this.select} index={index} />}
                         keyExtractor={(item, index) => item + index}
                         numColumns={this.state.numColumns}
                         key={this.state.numColumns}
@@ -83,6 +83,14 @@ class Gallery extends Component {
 
     removeSelected = () => {
 
+    }
+
+    select = (i) => {
+        var photosList = JSON.parse(JSON.stringify(this.state.photos))
+        photosList[i].toDelete = photosList[i].toDelete ? false : true
+        this.setState({
+            photos: photosList
+        })
     }
 }
 
