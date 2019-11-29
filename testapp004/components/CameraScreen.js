1d1c91287164cd5c7ccee1468ebd91cc4901ae43
lukaszkot03@gmail.com <lukaszkot03@gmail.com>
2019-11-29 14:27:44 +0100

    Added camera settings.

--- testapp004/components/CameraScreen.js
+++ testapp004/components/CameraScreen.js
@@ -16,14 +16,18 @@ class CameraScreen extends Component {
             hasCameraPermission: null,
             type: Camera.Constants.Type.back,
             takenPhotos: [],
-            wb: 0,
+            wb: "auto",
+            fm: "off",
+            ratios: ["4:3", "16:9"],
+            ratio: "16:9",
+            ps: "1920x1080",
+            sizes: ["1920x1080"],
             pos: new Animated.Value(Dimensions.get("window").height),
         };
         this.isHidden = true
     }
 
     toggle() {
-
         if (this.isHidden) toPos = 0; else toPos = Dimensions.get("window").height
 
         Animated.spring(
@@ -37,6 +41,7 @@ class CameraScreen extends Component {
         ).start();
 
         this.isHidden = !this.isHidden;
+        this.reloadResolutions();
     }
 
     handleBackPress = () => {
@@ -55,6 +60,15 @@ class CameraScreen extends Component {
         this.backHandler.remove()
     }
 
+    reloadResolutions = async () => {
+        if (this.camera) {
+            this.setState({
+                sizes: await this.camera.getAvailablePictureSizesAsync(this.state.ratio)
+            })
+
+        }
+    }
+
     render() {
         const { hasCameraPermission } = this.state;
         if (hasCameraPermission == null) {
@@ -64,13 +78,19 @@ class CameraScreen extends Component {
         } else {
             return (
                 <View style={{ flex: 1 }}>
-                    <Camera ref={ref => { this.camera = ref; }} style={{ flex: 1 }} type={this.state.type} whiteBalance={this.state.wb}>
+                    <Camera ref={ref => { this.camera = ref; }} style={{ flex: 1 }} type={this.state.type} whiteBalance={this.state.wb} flashMode={this.state.fm} ratio={this.state.ratio} pictureSize={this.state.ps}>
                         <Animated.View
                             style={[styles.animatedView, { transform: [{ translateY: this.state.pos }] }]} >
                             <ScrollView>
                                 <Text style={{ fontSize: 20, fontWeight: "bold", marginTop: 20, marginHorizontal: 10, color: "white" }}>SETTINGS</Text>
                                 <RadioGroup data={Object.keys(Camera.Constants.WhiteBalance)} color="#F44336" direction="column"
-                                    groupName="WHITE BALANCE" selectedIndex={0} change={(i) => this.setState({ wb: Object.keys(Camera.Constants.WhiteBalance)[i] })} />
+                                    groupName="WHITE BALANCE" selectedIndex={Object.keys(Camera.Constants.WhiteBalance).indexOf(this.state.wb)} change={(i) => this.setState({ wb: Object.keys(Camera.Constants.WhiteBalance)[i] })} />
+                                <RadioGroup data={Object.keys(Camera.Constants.FlashMode)} color="#F44336" direction="column"
+                                    groupName="FLASH MODE" selectedIndex={Object.keys(Camera.Constants.FlashMode).indexOf(this.state.fm)} change={(i) => { this.setState({ fm: Object.keys(Camera.Constants.FlashMode)[i] }) }} />
+                                <RadioGroup data={this.state.ratios} color="#F44336" direction="column"
+                                    groupName="RATIO" selectedIndex={this.state.ratios.indexOf(this.state.ratio)} change={(i) => { this.setState({ ratio: this.state.ratios[i] }, () => { this.reloadResolutions(); this.setState({ ps: this.state.ratio == "16:9" ? "1920x1080" : "1440x1080" }) }); }} />
+                                <RadioGroup data={this.state.sizes} color="#F44336" direction="column"
+                                    groupName="RESOLUTIONS" selectedIndex={this.state.sizes.indexOf(this.state.ps)} change={(i) => { this.setState({ ps: this.state.sizes[i] }) }} />
                             </ScrollView>
                         </Animated.View>
                         <View style={{
